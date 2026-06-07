0f290e7456a8b59a8b85edbc7c9555885e35efe7
Bishal <visaladhikari2016@gmail.com>
2026-06-07 10:05:22 -0500

    Add back login page with cleaned text

--- homecare_erp/settings.py
+++ homecare_erp/settings.py
@@ -101,6 +101,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 
 GEMINI_API_KEY    = config('GEMINI_API_KEY',    default='')
